2ea09d75721ddb88d4f25472accf5763bbefd6b2
Daniel Rodriguez <drodrigo95@hotmail.com>
2023-07-19 14:27:58 -0500

    set up component for navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
 import { Routes, Route } from "react-router-dom";
+import './index.scss'
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>I am Shop Page</h1>
+      {/* <Outlet /> */}
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
     </Routes>
   );
 };
