5b499edf7fe1c8edda2391b30412da335721987b
Daniel Rodriguez <drodrigo95@hotmail.com>
2023-08-10 13:45:14 -0500

    finsished up all page functionality on front end minus the checkout

--- src/routes/shop/shop.component.js
+++ src/routes/shop/shop.component.js
@@ -1,19 +1,18 @@
-//import SHOP_DATA from '../../shop-data.json'
-//import { UserContext } from "../../components/context/user.context";
-import { ProductsContext } from "../../components/context/products.context";
-import { useContext } from "react";
-import ProductCard from "../../components/product-card/product-card.component";
-import './shop.styles.scss'
+// import { CategoriesContext } from "../../components/context/categories.context";
+// import { useContext } from "react";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
+import Category from "../category/category.component";
+import { Route, Routes } from "react-router-dom";
+import "./shop.styles.scss";
 
 const Shop = () => {
-  const { products } = useContext(ProductsContext);
+  //const { categoriesMap } = useContext(CategoriesContext);
 
   return (
-    <div className="products-container">
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </div>
+  <Routes>
+      <Route index element={<CategoriesPreview />} />
+      <Route path=":category" element={<Category />} />
+  </Routes>
   );
 };
 
