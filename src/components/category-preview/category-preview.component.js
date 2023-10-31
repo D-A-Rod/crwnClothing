10dad401924c2b7cc501ebb6cac62ee9e3a1ddb9
Daniel Rodriguez <drodrigo95@hotmail.com>
2023-10-31 13:02:27 -0500

    cat prev was commentted out

--- src/components/category-preview/category-preview.component.js
+++ src/components/category-preview/category-preview.component.js
@@ -1,50 +1,50 @@
-// import "./category-preview.styles.scss";
-// import ProductCard from "../product-card/product-card.component";
-// import { Link } from "react-router-dom";
-// // import Spinner from "../spinner/spinner.component";
-// // import {
-// //   selectCategiesMap,
-// //   selectCategoriesIsLoading,
-// // } from "../../store/categories/category.selector";
-// // import { useSelector } from "react-redux";
-// import { Fragment } from "react";
-// // import CategoriesPreview from "../../routes/categories-preview/categories-preview.component";
+import "./category-preview.styles.scss";
+import ProductCard from "../product-card/product-card.component";
+import { Link } from "react-router-dom";
+// import Spinner from "../spinner/spinner.component";
+// import {
+//   selectCategiesMap,
+//   selectCategoriesIsLoading,
+// } from "../../store/categories/category.selector";
+// import { useSelector } from "react-redux";
+import { Fragment } from "react";
+// import CategoriesPreview from "../../routes/categories-preview/categories-preview.component";
 
 
-// //                       { title, products }
-// const CategoryPreview = ({ title, products }) => {
-//   // const categoriesMap = useSelector(selectCategiesMap);
-//   // const isLoading = useSelector(selectCategoriesIsLoading);
+//                       { title, products }
+const CategoryPreview = ({ title, products }) => {
+  // const categoriesMap = useSelector(selectCategiesMap);
+  // const isLoading = useSelector(selectCategoriesIsLoading);
 
-//   return (
-//     <Fragment>
-//       <div className="category-preview-container">
-//       <h2>
-//         <Link to={title} className="title">{title.toUpperCase()}</Link>
-//       </h2>
-//       <div className="preview">
-//         {products
-//           .filter((_, idx) => idx < 4)
-//           .map((product) => (
-//             <ProductCard key={product.id} product={product} />
-//           ))}
-//       </div>
-//     </div>
+  return (
+    <Fragment>
+      <div className="category-preview-container">
+      <h2>
+        <Link to={title} className="title">{title.toUpperCase()}</Link>
+      </h2>
+      <div className="preview">
+        {products
+          .filter((_, idx) => idx < 4)
+          .map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </div>
 
-//       {/* {isLoading ? (
-//         <Spinner />
-//       ) : (
-//         Object.keys(categoriesMap).map((title) => {
-//           const products = categoriesMap[title];
-//           return (
-//             <CategoriesPreview key={title} title={title} products={products} />
-//           );
-//         })
-//       )} */}
-//     </Fragment>
-//   );
-// };
+      {/* {isLoading ? (
+        <Spinner />
+      ) : (
+        Object.keys(categoriesMap).map((title) => {
+          const products = categoriesMap[title];
+          return (
+            <CategoriesPreview key={title} title={title} products={products} />
+          );
+        })
+      )} */}
+    </Fragment>
+  );
+};
 
-// export default CategoryPreview;
+export default CategoryPreview;
 
 
